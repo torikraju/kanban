923264d3aafb377046d292c615af01e1136b1762
Torikul Alam <torikraju@gmail.com>
2019-09-16 21:31:25 +0600

    Refactor Project Controller

--- kanban-api/src/main/java/edu/torikraju/kanban_api/controllers/ProjectController.java
+++ kanban-api/src/main/java/edu/torikraju/kanban_api/controllers/ProjectController.java
@@ -2,33 +2,38 @@ package edu.torikraju.kanban_api.controllers;
 
 
 import edu.torikraju.kanban_api.domain.Project;
+import edu.torikraju.kanban_api.services.MapValidationErrorService;
 import edu.torikraju.kanban_api.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
     private ProjectService projectService;
+    private MapValidationErrorService errorService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, MapValidationErrorService errorService) {
         this.projectService = projectService;
+        this.errorService = errorService;
     }
 
     @PostMapping("")
     public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            return new ResponseEntity<>("invalid object", HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = errorService.mapValidationService(result);
+        if (errorMap != null) return errorMap;
         Project newProject = projectService.saveOrUpdate(project);
         return new ResponseEntity<>(newProject, HttpStatus.CREATED);
     }
