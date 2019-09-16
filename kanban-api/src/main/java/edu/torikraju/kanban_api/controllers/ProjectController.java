6db7645dab95ab0f028932212ba735c44a3b1985
Torikul Alam <torikraju@gmail.com>
2019-09-16 21:09:39 +0600

    Project Object Validation

--- kanban-api/src/main/java/edu/torikraju/kanban_api/controllers/ProjectController.java
+++ kanban-api/src/main/java/edu/torikraju/kanban_api/controllers/ProjectController.java
@@ -6,10 +6,13 @@ import edu.torikraju.kanban_api.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,7 +25,10 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+    public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("invalid object", HttpStatus.BAD_REQUEST);
+        }
         Project newProject = projectService.saveOrUpdate(project);
         return new ResponseEntity<>(newProject, HttpStatus.CREATED);
     }
